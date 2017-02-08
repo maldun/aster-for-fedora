9349ece45fb85567570eec0c4732b86e298483ea
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-08 20:59:10 +0100

    simplified script

--- hdf5.sh
+++ hdf5.sh
@@ -4,12 +4,5 @@
 
 source "$(dirname "$0")"/variables.sh
 
-cd ${DOWNL}
-cd aster-full-src-${ASTER_SUB}/SRC
-
-tar -xvf ${HDF}.tar.gz
-mv ${HDF} codeaster-${HDF}
-tar cvzf codeaster-${HDF}.tar.gz codeaster-${HDF}
-cp codeaster-${HDF}.tar.gz ${SOURCE_DIR}
-cd ${SPEC_DIR}
+copy_pkg ${HDF} ${HDF}
 QA_SKIP_BUILD_ROOT=1 rpmbuild -ba codeaster-hdf5.spec
