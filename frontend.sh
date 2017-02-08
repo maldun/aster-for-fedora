f02e6f6375b0aaa77a7395dcec0caa911ec8f08a
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-08 21:20:25 +0100

    simplified script

--- frontend.sh
+++ frontend.sh
@@ -4,16 +4,17 @@
 
 source "$(dirname "$0")"/variables.sh
 export FRONTEND_FULL_VER=${FRONTEND_VER}-${FRONTEND_SUB}
+export FRONTEND=codeaster-frontend-${FRONTEND_VER}
 
 cd ${DOWNL}
 cd aster-full-src-${ASTER_SUB}/SRC
 
 #Rename package
 tar -xvf astk-${FRONTEND_FULL_VER}.tar.gz
-mv astk-${FRONTEND_VER} codeaster-frontend-${FRONTEND_VER}
-tar cvzf codeaster-${FRONTEND_VER}.tar.gz codeaster-frontend-${FRONTEND_VER}
+mv astk-${FRONTEND_VER} ${FRONTEND}
+tar cvzf ${FRONTEND}.tar.gz ${FRONTEND}
 
-cp codeaster-${FRONTEND_VER}.tar.gz ${SOURCE_DIR}
+cp ${FRONTEND}.tar.gz ${SOURCE_DIR}
 cd ${SPEC_DIR}
 
 QA_SKIP_BUILD_ROOT=1 rpmbuild -ba codeaster-frontend.spec
