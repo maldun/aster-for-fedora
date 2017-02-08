6b7a4edb165883c016f12558f4790af9e9acbb71
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-08 21:51:10 +0100

    added download

--- download.sh
+++ download.sh
@@ -10,23 +10,25 @@ source "$(dirname "$0")"/variables.sh
 mkdir -p ${ASTER_BASE}
 mkdir -p ${EXTLIBS}
 
-################
-# yum packages #
-################
-#yum -y install glibc-static zlib zlib-static krb5-devel python2-devel libstdc++-static blas lapack flex tk numpy
-
-
 #########################
 # Downloads
 #########################
 
 cd ${DOWNL}
 #wget http://www.code-aster.org/FICHIERS/aster-full-src-${ASTER_FULL}.noarch.tar.gz
+wget http://ftp.tu-chemnitz.de/pub/linux/dag/redhat/el7/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
+
+
+################
+# yum packages #
+################
+#yum -y install glibc-static zlib zlib-static krb5-devel python2-devel libstdc++-static blas lapack flex tk numpy
+rpm -Uvh rpmforge-release*rpm
 
 #########################
 # Unpack
 #########################
 
-tar -xvf ${ASTER_FULL}
-cd aster-full-src-${ASTER_SUB}/SRC
+#tar -xvf ${ASTER_FULL}
+#cd aster-full-src-${ASTER_SUB}/SRC
 
