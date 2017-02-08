4f932d6957846b1cf11a78856d1c4c65a9b2cc9c
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-08 22:15:24 +0100

    trick 17

--- mumps-stable.sh
+++ mumps-stable.sh
@@ -5,6 +5,6 @@
 source "$(dirname "$0")"/variables.sh
 export MUMPS_STABLE_FULL=${MUMPS_STABLE}-${MUMPS_STABLE_SUB}
 
-copy_pkg ${MUMPS_STABLE} ${MUMPS_STABLE_FULL}
+copy_pkg stable-${MUMPS_STABLE} ${MUMPS_STABLE_FULL}
 
 QA_SKIP_BUILD_ROOT=1 rpmbuild -ba codeaster-mumps-stable.spec
