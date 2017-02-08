2984e4f2fcc336fc9c507eb80222f756bece1c07
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-08 22:27:00 +0100

    further naming

--- mumps-stable.sh
+++ mumps-stable.sh
@@ -7,4 +7,8 @@ export MUMPS_STABLE_FULL=${MUMPS_STABLE}-${MUMPS_STABLE_SUB}
 
 copy_pkg ${MUMPS_STABLE} ${MUMPS_STABLE_FULL}
 
+cd ${SOURCE_DIR}
+mv codeaster-${MUMPS_STABLE} codeaster-stable-${MUMPS_STABLE}
+cd ${SPEC_DIR}
+
 QA_SKIP_BUILD_ROOT=1 rpmbuild -ba codeaster-stable-mumps.spec
