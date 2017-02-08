9d0610160fda961be2a823903188fce3c86d710b
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-08 20:02:04 +0100

    added buildscript

--- build_openblas.sh
+++ build_openblas.sh
@@ -1,7 +1,11 @@
 source "$(dirname "$0")"/variables.sh
 
 mkdir -p ${EXTLIBS}
-cd ${EXTLIBS}
+cd ${DOWNL}
 git clone https://github.com/xianyi/OpenBLAS.git OpenBLAS
 cd OpenBLAS
 make NO_AFFINITY=1 USE_OPENMP=1 FC=gfortran
+make PREFIX=${EXTLIBS}/OpenBLAS install
+echo /opt/OpenBLAS/lib | sudo tee -a /etc/ld.so.conf.d/openblas.conf
+sudo ldconfig
+
