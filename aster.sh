2d2baa83dbd33d0d0633d97fe7707c5925652d8d
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-09 23:22:55 +0100

    changed aster script

--- aster.sh
+++ aster.sh
@@ -5,7 +5,13 @@
 source "$(dirname "$0")"/variables.sh
 export ASTER_STABLE_FULL=${ASTER_STABLE}.${ASTER_STABLE_SUB}
 
-copy_pkg_type ${ASTER_STABLE_FULL} aster-${ASTER_STABLE_FULL} stable tgz
+cd ${DOWNL}
+cd aster-full-src-${ASTER_SUB}/SRC
 
+tar -xvf aster-${ASTER_STABLE_FULL}.tgz
+mv aster-${ASTER_STABLE_FULL} codeaster-stable-${ASTER_STABLE_FULL}
+tar cvzf codeaster-stable-${ASTER_STABLE_FULL}.tar.gz codeaster-stable-${ASTER_STABLE_FULL}
+cp codeaster-stable-${ASTER_STABLE_FULL}.tar.gz ${SOURCE_DIR}
+cd ${SPEC_DIR}
 
 QA_SKIP_BUILD_ROOT=1 rpmbuild -ba codeaster-stable.spec
