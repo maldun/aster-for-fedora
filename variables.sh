9a78fe2614a2aa54b07535119930bc25e10f3f76
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-09 23:17:11 +0100

    naming

--- variables.sh
+++ variables.sh
@@ -40,7 +40,7 @@ export MUMPS_STABLE=mumps-4.10.0
 export MUMPS_STABLE_SUB=aster3-2
 export MFRONT=mfront-2.0.3
 export MFRONT_SUB=1
-export ASTER_STABLE=aster-12.7
+export ASTER_STABLE=12.7
 export ASTER_STABLE_SUB=0
 
 ###################
