5b777c590fdc2d8b256365191b6fdd4625c30c2e
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-15 23:17:52 +0100

    corrected versioning

--- scalapack.sh
+++ scalapack.sh
@@ -6,7 +6,7 @@ source "$(dirname "$0")"/variables.sh
 cd ${DOWNL}
 mkdir codeaster-scalapack-openmpi-${SCALAPACK}
 tar -xvf scalapack_installer.tgz
-mv scalapack_installer codeaster-scalapack-openmpi-${SCALAPACK}/scalapack_installer-${SCALAPACK_INSTALLER}
+mv scalapack_installer codeaster-scalapack-openmpi-${SCALAPACK}/scalapack_installer_${SCALAPACK_INSTALLER}
 tar cvzf codeaster-scalapack-openmpi-${SCALAPACK}.tar.gz codeaster-scalapack-openmpi-${SCALAPACK}
 cp codeaster-scalapack-openmpi-${SCALAPACK}.tar.gz ${SOURCE_DIR}
 cd ${SPEC_DIR}
