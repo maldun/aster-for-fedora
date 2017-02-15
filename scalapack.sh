8ba2f0b4f8d2737f17dc8d16d91f7a35172e56f2
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-15 23:03:03 +0100

    manual script for scalapack

--- scalapack.sh
+++ scalapack.sh
@@ -4,8 +4,9 @@
 
 source "$(dirname "$0")"/variables.sh
 cd ${DOWNL}
-mv scalapack_installer.tgz aster-full-src-${ASTER_SUB}/SRC
-
-copy_pkg_type scalapack scalapack_installer openmpi-${SCALAPACK} tgz
+tar -xvf scalapack_installer.tgz
+tar cvzf codeaster-scalapack-openmpi-${SCALAPACK}.tar.gz scalapack_installer
+cp codeaster-scalapack-openmpi-${SCALAPACK}.tar.gz ${SOURCE_DIR}
+cd ${SPEC_DIR}
 
 QA_SKIP_BUILD_ROOT=1 rpmbuild -ba codeaster-scalapack-openmpi.spec
