19bdb26c75ded65047c333e726b908295d8abf46
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-15 22:56:35 +0100

    correction for file type

--- scalapack.sh
+++ scalapack.sh
@@ -4,6 +4,6 @@
 
 source "$(dirname "$0")"/variables.sh
 
-copy_pkg_type scalapack scalapack_installer openmpi-${SCALAPACK}
+copy_pkg_type scalapack scalapack_installer openmpi-${SCALAPACK} tgz
 
 QA_SKIP_BUILD_ROOT=1 rpmbuild -ba codeaster-scalapack-openmpi.spec
