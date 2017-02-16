8aae677968fb8a76b9b38e554168108b5d86caff
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-16 20:28:41 +0100

    changes petsc script (hack ...)

--- petsc.sh
+++ petsc.sh
@@ -7,6 +7,9 @@ export PETSC_STABLE=petsc-${PETSC_STABLE_VER}
 export MUMPS_STABLE_MPI=stable-petsc-openmpi-${PETSC_STABLE_VER}
 export PATH=/cad/app/openmpi/1.10.5/bin:$PATH
 
+cd ${DOWNL}
+mv ${PETSC_STABLE}.tar.gz aster-full-src-${ASTER_SUB}/SRC/
+
 copy_pkg_type_mpi ${PETSC_STABLE} ${PETSC_STABLE} ${PETSC_STABLE_MPI} tar.gz
 
 
