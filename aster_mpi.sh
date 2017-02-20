bccc55e17270fe961fe8cc902a794e2f412b0976
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-20 20:49:26 +0100

    corrected mpi path

--- aster_mpi.sh
+++ aster_mpi.sh
@@ -8,10 +8,10 @@ export ASTER_STABLE_FULL=${ASTER_STABLE}.${ASTER_STABLE_SUB}
 cd ${DOWNL}
 cd aster-full-src-${ASTER_SUB}/SRC
 
-tar -xvf aster-${ASTER_STABLE_FULL}.tgz
-mv aster-${ASTER_STABLE_FULL} codeaster-stable-openmpi-${ASTER_STABLE_FULL}
-tar cvzf codeaster-stable-openmpi-${ASTER_STABLE_FULL}.tar.gz codeaster-stable-openmpi-${ASTER_STABLE_FULL}
-cp codeaster-stable-openmpi-${ASTER_STABLE_FULL}.tar.gz ${SOURCE_DIR}
+#tar -xvf aster-${ASTER_STABLE_FULL}.tgz
+#mv aster-${ASTER_STABLE_FULL} codeaster-stable-openmpi-${ASTER_STABLE_FULL}
+#tar cvzf codeaster-stable-openmpi-${ASTER_STABLE_FULL}.tar.gz codeaster-stable-openmpi-${ASTER_STABLE_FULL}
+#cp codeaster-stable-openmpi-${ASTER_STABLE_FULL}.tar.gz ${SOURCE_DIR}
 cd ${SPEC_DIR}
 
 export LD_LIBRARY_PATH=/cad/app/aster/public/mfront-2.0.3/lib:$LD_LIBRARY_PATH
