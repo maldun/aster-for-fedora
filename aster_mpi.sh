46cbb5b580423cebf26b1d1c7391eced669bcbfe
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-20 21:01:13 +0100

    added variable

--- aster_mpi.sh
+++ aster_mpi.sh
@@ -14,7 +14,7 @@ cd aster-full-src-${ASTER_SUB}/SRC
 #cp codeaster-stable-openmpi-${ASTER_STABLE_FULL}.tar.gz ${SOURCE_DIR}
 cd ${SPEC_DIR}
 
-export LD_LIBRARY_PATH=/cad/app/aster/public/mfront-2.0.3/lib:$LD_LIBRARY_PATH
-export PATH=/cad/app/aster/public/mfront-2.0.3/bin:$PATH
+export LD_LIBRARY_PATH=/cad/app/openmpi/1.10.5/lib:/cad/app/aster/public/mfront-2.0.3/lib:$LD_LIBRARY_PATH
+export PATH=/cad/app/openmpi/1.10.5/bin:/cad/app/aster/public/mfront-2.0.3/bin:$PATH
 
 QA_SKIP_BUILD_ROOT=1 rpmbuild -ba codeaster-stable-openmpi.spec
