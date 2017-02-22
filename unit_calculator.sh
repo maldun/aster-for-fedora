b25731afc055d2c02761ff3a64956face5303b6e
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-22 23:10:49 +0100

    corrected path

--- unit_calculator.sh
+++ unit_calculator.sh
@@ -1,2 +1,8 @@
+#########################
+# UNIT CALCULATOR (opt)
+#########################
+
+source "$(dirname "$0")"/variables.sh
+
 cd ${EXTLIBS}
 git clone https://github.com/maldun/UnitCalculator.git
