01606c25e588700c0dfb0bac0448ef6b7f13273a
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-27 22:50:46 +0100

    changed order of variables

--- med.sh
+++ med.sh
@@ -6,4 +6,4 @@ source "$(dirname "$0")"/variables.sh
 
 copy_pkg ${MED} ${MED}
 
-QA_SKIP_BUILD_ROOT=1 rpmbuild -ba codeaster-med.spec --define "version $MED_VER"
+QA_SKIP_BUILD_ROOT=1 rpmbuild -ba codeaster-med.spec --define "version $MED_VER" --define "hdf5_version $HDF_VER" --define "aster_libs $ASTER_LIBS" --define "aster_root $ASTER_BASE"
