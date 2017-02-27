db2c063c172873ba42f13a22e0403d8a7c446b83
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-02-27 20:37:28 +0100

    redo defines

--- hdf5.sh
+++ hdf5.sh
@@ -5,4 +5,4 @@
 source "$(dirname "$0")"/variables.sh
 
 copy_pkg ${HDF} ${HDF}
-QA_SKIP_BUILD_ROOT=1 rpmbuild --define "aster_root $ASTER_BASE" "aster_libs $ASTER_LIBS" "openblas_dir $OPENBLAS_DIR"-ba codeaster-hdf5.spec
+QA_SKIP_BUILD_ROOT=1 rpmbuild --define "aster_root $ASTER_BASE" --define "aster_libs $ASTER_LIBS" --define "openblas_dir $OPENBLAS_DIR"-ba codeaster-hdf5.spec
