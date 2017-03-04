76eeb22d29a132f50cdac93dab6e052aaed2dc17
Stefan Reiterer <stefan.harald.reiterer@gmail.com>
2017-03-04 11:47:40 +0100

    commented part out for testing reasons

--- aster.sh
+++ aster.sh
@@ -5,13 +5,13 @@
 source "$(dirname "$0")"/variables.sh
 export ASTER_STABLE_FULL=${ASTER_STABLE}.${ASTER_STABLE_SUB}
 
-cd ${DOWNL}
-cd aster-full-src-${ASTER_SUB}/SRC
-
-tar -xvf aster-${ASTER_STABLE_FULL}.tgz
-mv aster-${ASTER_STABLE_FULL} codeaster-stable-${ASTER_STABLE_FULL}
-tar cvzf codeaster-stable-${ASTER_STABLE_FULL}.tar.gz codeaster-stable-${ASTER_STABLE_FULL}
-cp codeaster-stable-${ASTER_STABLE_FULL}.tar.gz ${SOURCE_DIR}
+# cd ${DOWNL}
+# cd aster-full-src-${ASTER_SUB}/SRC
+# 
+# tar -xvf aster-${ASTER_STABLE_FULL}.tgz
+# mv aster-${ASTER_STABLE_FULL} codeaster-stable-${ASTER_STABLE_FULL}
+# tar cvzf codeaster-stable-${ASTER_STABLE_FULL}.tar.gz codeaster-stable-${ASTER_STABLE_FULL}
+# cp codeaster-stable-${ASTER_STABLE_FULL}.tar.gz ${SOURCE_DIR}
 cd ${SPEC_DIR}
 
 export LD_LIBRARY_PATH=${ASTER_LIBS}/${MFRONT}/lib:$LD_LIBRARY_PATH
